2f0232681dceb38e29384404753bad8d0f39691b
Erik Einarsson <enriz@enriz.se>
2017-09-14 00:44:45 +0200

    Fixed iframe reload
    Enabled iframe navigation

--- SBF-fixer.user.js
+++ SBF-fixer.user.js
@@ -4,19 +4,17 @@
 // @version      0.1
 // @description  Clean up other peoples mess
 // @author       Erik Einarsson
-// @require https://code.jquery.com/jquery-2.1.4.min.js
-// @include        http://localhost/sbf/* //For testing purposes only
-// @include        http://www.svenskbridge.se/tvl/*/info
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @include      http://www.svenskbridge.se/tvl/*/info
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    // Your code here...
-$(window).ready(fixEverything());
-
-function fixEverything(){
+//Resize iFrame of "spader" class
+var myiFrame = $( ".spader" );
+myiFrame.load(function(){
+    console.log("Resized your iframe, you're welcome. //Erik Einarsson");
     $( ".spader" ).height("1000px");
-}
+});
 })();
