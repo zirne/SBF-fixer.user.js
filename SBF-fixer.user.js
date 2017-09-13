139f72ecc7e30bd2aebf90cc81c9b20f7c00f552
Erik Einarsson <enriz@enriz.se>
2017-09-14 01:34:28 +0200

    Edited script description

--- SBF-fixer.user.js
+++ SBF-fixer.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         SBF-fixer
 // @namespace    https://code.enriz.eu
-// @version      0.1.5
-// @description  Clean up other peoples mess
+// @version      0.1.6
+// @description  Fixing broken things over at svenskbridge.se, check out my code over at github.com/zirne
 // @author       Erik Einarsson
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
 // @match        http://www.svenskbridge.se/tvl/*/info
@@ -15,7 +15,7 @@
 //Resize iFrame of "spader" class
 var myiFrame = $( ".spader" );
 myiFrame.load(function(){
-    console.log("Resized your iframe, you're welcome. //Erik Einarsson");
+    console.log("Resized your iframe, you're welcome. //Erik Einarsson. If you are reading this, you are cute. <3");
     $( ".spader" ).height("1000px");
 });
 })();
